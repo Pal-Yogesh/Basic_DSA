1f3cef96bc90047bcfae7164cc79db8c22b09f75
Pal-Yogesh <yogeshpal5049@gmail.com>
2022-04-18 17:01:27 +0530

    Collection_framework using HashMap

---
+++ src/Coll_fwok.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Coll_fwok {
+    
+    public static void main(String[] args) {
+        HashMap<Integer,Integer>obj = new HashMap<>();
+        obj.put(10,100);
+        System.out.println(obj.get(10));
+        System.out.println(obj.getOrDefault(10,0));
+        for(Map.Entry<Integer,Integer>Res:obj.entrySet()){
+            System.out.println(Res.getKey());
+            System.out.println(Res.getValue());
+        }
+    }
+}
